25891119f2f8feb96c0d60eb528fe7938698a3f6
Arun Chandran <Arun.Chandran@amd.com>
2024-04-30 15:30:52 +0530

    ARCH/X86: use division instead of shifting to calculate thresholds

--- src/ucs/arch/x86_64/cpu.c
+++ src/ucs/arch/x86_64/cpu.c
@@ -595,7 +595,7 @@ static size_t ucs_cpu_nt_bt_thresh_min(size_t user_val)
     }
 
     if ((ucs_arch_get_cpu_vendor() == UCS_CPU_VENDOR_AMD)) {
-        return ((ucs_cpu_get_cache_size(UCS_CPU_CACHE_L3) * 3) >> 2);
+        return ((ucs_cpu_get_cache_size(UCS_CPU_CACHE_L3) * 3) / 4);
     } else {
         return UCS_MEMUNITS_INF;
     }
@@ -604,7 +604,7 @@ static size_t ucs_cpu_nt_bt_thresh_min(size_t user_val)
 static size_t ucs_cpu_nt_dest_thresh()
 {
     if ((ucs_arch_get_cpu_vendor() == UCS_CPU_VENDOR_AMD)) {
-        return ((ucs_cpu_get_cache_size(UCS_CPU_CACHE_L3) * 29) >> 5);
+        return ((ucs_cpu_get_cache_size(UCS_CPU_CACHE_L3) * 29) / 32);
     } else {
         return UCS_MEMUNITS_INF;
     }

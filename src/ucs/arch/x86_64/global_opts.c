e61e3677c3c801c8b96b6e54c2961395cc6ac05c
Arun Chandran <Arun.Chandran@amd.com>
2026-09-12 13:27:02 +0200

    ARCH/X86: Clarify built-in memcpy status
    Signed-off-by: Arun Chandran <Arun.Chandran@amd.com>

--- src/ucs/arch/x86_64/global_opts.c
+++ src/ucs/arch/x86_64/global_opts.c
@@ -1,6 +1,6 @@
 /**
 * Copyright (c) NVIDIA CORPORATION & AFFILIATES, 2019. ALL RIGHTS RESERVED.
-* Copyright (C) Advanced Micro Devices, Inc. 2024. ALL RIGHTS RESERVED.
+* Copyright (C) Advanced Micro Devices, Inc. 2024-2026. ALL RIGHTS RESERVED.
 *
 * See file LICENSE for terms.
 */
@@ -13,6 +13,7 @@
 
 #include <ucs/arch/global_opts.h>
 #include <ucs/config/parser.h>
+#include <ucs/sys/string.h>
 
 ucs_config_field_t ucs_arch_global_opts_table[] = {
 #if ENABLE_BUILTIN_MEMCPY
@@ -43,12 +44,23 @@ void ucs_arch_print_memcpy_limits(ucs_arch_global_opts_t *config)
 
 #if ENABLE_BUILTIN_MEMCPY
     char max_thresh_str[32];
-    ucs_config_sprintf_memunits(min_thresh_str, sizeof(min_thresh_str),
-                                &config->builtin_memcpy_min, NULL);
-    ucs_config_sprintf_memunits(max_thresh_str, sizeof(max_thresh_str),
-                                &config->builtin_memcpy_max, NULL);
-    printf("# Using built-in memcpy() for size %s..%s\n",
-           min_thresh_str, max_thresh_str);
+
+    if (config->builtin_memcpy_max > config->builtin_memcpy_min) {
+        ucs_config_sprintf_memunits(min_thresh_str, sizeof(min_thresh_str),
+                                    &config->builtin_memcpy_min, NULL);
+        ucs_config_sprintf_memunits(max_thresh_str, sizeof(max_thresh_str),
+                                    &config->builtin_memcpy_max, NULL);
+        printf("# Using built-in memcpy() for size %s..%s\n",
+               min_thresh_str, max_thresh_str);
+    } else if ((config->builtin_memcpy_min != UCS_MEMUNITS_INF) &&
+               (config->nt_buffer_transfer_min != UCS_MEMUNITS_INF) &&
+               (config->nt_buffer_transfer_min <=
+                config->nt_dest_threshold)) {
+        printf("# Built-in memcpy() is enabled only within "
+               "nt-buffer-transfer\n");
+    } else {
+        printf("# Built-in memcpy() is disabled\n");
+    }
 #endif
 
     ucs_config_sprintf_memunits(min_thresh_str, sizeof(min_thresh_str),
